111d6140f5bd6d9852f32666b087b26009282df5
KaArTtHiK <sirigirikarthik00@gmail.com>
2021-06-10 12:28:34 +0530

    create_view

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,7 +1,8 @@
-from .views import TaskList,TaskDetail
+from .views import TaskList,TaskDetail,TaskCreate
 from django.urls import path
 
 urlpatterns = [
     path("",TaskList.as_view(), name='tasks'),
     path("task/<int:pk>/",TaskDetail.as_view(), name = 'task'),
+    path("task-create/",TaskCreate.as_view(), name='task-create'),
 ]
