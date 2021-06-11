10977bf746e77c3d07d94fb3fe8bc9d033ddedfd
KaArTtHiK <sirigirikarthik00@gmail.com>
2021-06-12 01:45:42 +0530

    Login_Logout_commit

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,7 +1,10 @@
-from .views import TaskList,TaskDetail,TaskCreate,TaskUpdate,DeleteView
+from .views import TaskList,TaskDetail,TaskCreate,TaskUpdate,DeleteView, CustomLoginView
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path("logout/",LogoutView.as_view(next_page = 'login'), name = "logout"),
+    path("login/",CustomLoginView.as_view(), name = "login"),
     path("",TaskList.as_view(), name='tasks'),
     path("task/<int:pk>/",TaskDetail.as_view(), name = 'task'),
     path("task-create/",TaskCreate.as_view(), name='task-create'),
